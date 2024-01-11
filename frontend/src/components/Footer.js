d516829cfa5ac2ae83decb2ca12c8cf0e5929cfa
Sanbedan Paul <sanbedan9989@gmail.com>
2024-01-11 14:26:31 +0530

    first commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div id="footer">
+      <span>
+        Designed and Developed with{` `}
+        <span className="emoji">
+          <i className="fas fa-heart"></i>{" "}
+        </span>{" "}
+        by Sanbedan Paul, Debjeet Banarjee , Rajdeep Bhattachary and Tirtho Raj
+        Ghosh
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/constants/Months.js
@@ -0,0 +1,50 @@
+export const monthsObject = [
+  {
+    month: "January",
+    id: "Baisakh",
+  },
+  {
+    month: "February",
+    id: "Jestha",
+  },
+  {
+    month: "March",
+    id: "Ashadh",
+  },
+  {
+    month: "April",
+    id: "Shrawan",
+  },
+  {
+    month: "May",
+    id: "Bhadra",
+  },
+  {
+    month: "June",
+    id: "Ashoj",
+  },
+  {
+    month: "July",
+    id: "Kartik",
+  },
+  {
+    month: "August",
+    id: "Mangsir",
+  },
+  {
+    month: "September",
+    id: "Poush",
+  },
+  {
+    month: "October",
+    id: "Magh",
+  },
+  {
+    month: "November",
+    id: "Falgun",
+  },
+  {
+    month: "December",
+    id: "Chaitra",
+  },
+];
